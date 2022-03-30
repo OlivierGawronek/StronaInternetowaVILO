64ecedbc02f6ef8fea46249482c5f2a038798b9b
OlivierGawronek <78927406+OlivierGawronek@users.noreply.github.com>
2022-03-30 22:13:07 +0200

    Poprawki

--- Game.js
+++ Game.js
@@ -1,5 +1,7 @@
 class Game{
     constructor(conf) {
         this.element = conf.element;
+        this.canvas = this.element.querySelector(".ContGry");
+        this.ctx = this.canvas.getContent("2d")
     }
 }
