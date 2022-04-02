eeae8e2ae37bef9798205e94b6b2425f1e5d9f11
krzys938 <krzys938@gmial.com>
2022-04-02 18:37:47 +0200

    Dodanie make2DArray

--- Game.js
+++ Game.js
@@ -14,7 +14,14 @@ class Game{
         mainChar.onload = () => {this.ctx.drawImage(mainChar, 0, 0)};
         mainChar.src = "images/boh1.png";
 
-
     }
 
+}
+
+function make2DArray(cols, rows) {
+    let arr = new Array(cols);
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = new Array(rows);
+    }
+    return arr;
 }
